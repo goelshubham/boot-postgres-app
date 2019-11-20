cc5edc6ee003c791cdea8931c0e9dcd8d7a5fc99
goels10 <shubham.goel@dell.com>
2019-11-19 21:56:38 -0500

    first commit

---
+++ src/main/java/com/bootpostgresapp/BootPostgresAppApplication.java
@@ -0,0 +1,13 @@
+package com.bootpostgresapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPostgresAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPostgresAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootpostgresapp/BootPostgresAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootpostgresapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootPostgresAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
